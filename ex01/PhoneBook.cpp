ecb5228fc26da8f55e9ac2ca4a71975397eb7722
Marianna Zhukova <mzhukova@c1r2s1.42wolfsburg.de>
2024-09-19 21:17:39 +0200

    cleaned the code

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -43,26 +43,21 @@ void PhoneBook::save_input(std::string value, std::string& placeholder)
 
 void PhoneBook::search_contact(void)
 {
-	std::string first_name;
-	std::string last_name;
-	std::string nickname;
+	std::string index;
 	int i = 0;
 	//display_contacts
 	std::cout << CYAN << "CURRENT CONTACTS:" << RESET << std::endl;
 	while (i < 8)
 	{
 		std::cout << "        " << _contact_list[i].index << " | ";
-		first_name = _contact_list[i].first_name;
-		display_in_column(first_name);
-		last_name = _contact_list[i].last_name;
-		display_in_column(last_name);
-		nickname = _contact_list[i].nickname;
-		display_in_column(nickname);
+		display_in_column(_contact_list[i].first_name);
+		display_in_column(_contact_list[i].last_name);
+		display_in_column(_contact_list[i].nickname);
 		std::cout << std::endl;
 		i++;
 	}
-	
-
+	std::cout << "Enter the index of the contact:" << std::endl;
+	std::getline(std::cin, index);
 }
 void PhoneBook::display_in_column(std::string field_name)
 {
@@ -78,17 +73,3 @@ void PhoneBook::display_in_column(std::string field_name)
 	std::cout << field_name <<  " | ";
 	
 }
-
-//add a function to push contacts
-	//if more that 8 contacts already, replace the oldest one with the new one)
-	//(set a variable for an oldest index)
-//function add_contact()
-	//asks for prompt one by one
-	//The contact fields are: first name, last name, nickname, phone number, and
-	//darkest secret. A saved contact can’t have empty fields.
-//function search_contact()
-	//display weird columns (index, firstname, last_name, nickname)
-	//ask for an index
-	//if correct index -> display the contact info one line at a time
-//function exit
-	//quits
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -17,10 +17,8 @@ int main()
 		else if (!input_cmd.compare("ADD"))
 		{
 			my_phone_book.add_contact();
-			//my_phone_book.counter++;
 		}
 		else if (!input_cmd. compare("SEARCH"))
-			// std::cout << "Lets search some contacts" << std::endl;
 			my_phone_book.search_contact();
 	}
 	return (0);
