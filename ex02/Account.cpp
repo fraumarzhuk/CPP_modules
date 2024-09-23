f8516356b224ed0decb5f64c110a0c3cd28f9f22
Marianna Zhukova <mzhukova@c1r1s3.42wolfsburg.de>
2024-09-23 20:05:04 +0200

    changes in phonebook and in account

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,14 +1,37 @@
 #include "Account.hpp"
 
-Account::Account(int initial_deposit)
+Account::Account(int initial_deposit) : _amount(initial_deposit)
 {
-	this->_totalAmount = initial_deposit;
+	_totalAmount = initial_deposit;
+	_accountIndex = _nbAccounts;
+	_nbAccounts += 1;
+	_displayTimestamp();
+	std::cout << "index:" << RED << _accountIndex << RESET << ";";
+	std::cout << "amount:" << RED << _amount << RESET << ";";
+	std::cout << "created" << std::endl;
+}
+Account::Account(void)
+{
+	return ;
 }
 
 Account::~Account(void)
 {
-	return ;
+	_displayTimestamp();
+	std::cout << "index:" << RED << _accountIndex << RESET << ";";
+	std::cout << "amount:" << RED << _amount << RESET << ";";
+	std::cout << "closed" << std::endl;
 }
+
+void	Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << RED << _accountIndex << RESET << ";";
+	std::cout << "amount:" << RED << _amount << RESET << ";";
+	std::cout << "deposits:" << RED << _amount << RESET << ";";
+	std::cout << "withdrawals:" << RED << _amount << RESET << std::endl;
+}
+
 int	 Account::getNbAccounts( void )
 {
 	return (Account::_nbAccounts);
@@ -28,32 +51,57 @@ int	Account::getNbWithdrawals( void )
 {
 	return (Account::_totalNbWithdrawals);
 }
+
+int		Account::checkAmount( void ) const
+{
+	return (_amount);
+}
+
 void	Account::displayAccountsInfos( void )
 {
-	std::cout << "index: "<< RED << this->_accountIndex << RESET << ";";
-	std::cout << "amount: "<< RED << Account::_totalAmount << RESET << ";";
-	std::cout << "deposits: "<< RED << this->_nbDeposits << RESET << ";";
-	std::cout << "withdrawals: "<< RED << this->_nbWithdrawals<< RESET << ";";
+	_displayTimestamp();
+	std::cout << "accounts:"<< RED << _nbAccounts << RESET << ";";
+	std::cout << "total:"<< RED << _totalAmount << RESET << ";";
+	std::cout << "deposits:"<< RED << _totalNbDeposits << RESET << ";";
+	std::cout << "withdrawals:"<< RED << _totalNbWithdrawals << RESET << std::endl;
 }
 
 void	Account::makeDeposit( int deposit )
 {
+	_displayTimestamp();
 	std::cout << "index:"<< RED << this->_accountIndex << RESET << ";";
-	std::cout << "p_amount:"<< RED << Account::_totalAmount << RESET << ";";
+	std::cout << "p_amount:"<< RED << _totalAmount << RESET << ";";
 	std::cout << "deposit:"<< RED << deposit << RESET << ";";
 	this->_nbDeposits += 1;
 	this->_totalAmount += deposit;
-	std::cout << "amount:"<< RED << Account::_totalAmount << RESET << ";";
-	std::cout << "nb_deposits:"<< RED << this->_nbDeposits<< RESET << ";";
+	std::cout << "amount:"<< RED << _totalAmount << RESET << ";";
+	std::cout << "nb_deposits:"<< RED << this->_nbDeposits<< RESET << std::endl;
 }
 
-void	makeWithdrawal( int withdrawal )
+bool	Account::makeWithdrawal( int withdrawal )
 {
+	_displayTimestamp();
 	std::cout << "index:"<< RED << this->_accountIndex << RESET << ";";
-	std::cout << "p_amount:"<< RED << Account::_totalAmount << RESET << ";";
-	std::cout << "withdrawal:"<< RED << withdrawal << RESET << ";";
-	this->nbWithdrawals += 1;
-	this->_totalAmount -= withdrawal;
-	std::cout << "amount:"<< RED << Account::_totalAmount << RESET << ";";
-	std::cout << "nb_withdrawals:"<< RED << this->_nbWithdrawals<< RESET << ";";
+	std::cout << "p_amount:"<< RED << _totalAmount << RESET << ";";
+	if (withdrawal <= _amount)
+	{
+		std::cout << "withdrawal:"<< RED << withdrawal << RESET << ";";
+		this->_totalNbWithdrawals += 1;
+		this->_totalAmount -= withdrawal;
+		std::cout << "amount:"<< RED << _totalAmount << RESET << ";";
+		std::cout << "nb_withdrawals:"<< RED << this->_nbWithdrawals<< RESET << std::endl;;
+		return (true);
+	}
+	else
+	{
+		std::cout << "withdrawal:refused" << std::endl;
+		return (false);
+	}
+}
+void Account::_displayTimestamp(void)
+{
+	time_t current_time = time(NULL);
+	struct tm *local_time = localtime(&current_time);
+	std::cout << '[' << local_time->tm_year + 1900 << local_time->tm_mon << local_time->tm_mday
+	<< "_" << local_time->tm_hour << local_time->tm_min << local_time->tm_sec << "] ";
 }
