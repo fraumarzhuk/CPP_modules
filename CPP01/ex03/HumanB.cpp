26035106356bac31b18e4034c8bfb28b166e4a69
fraumarzhuk <fraumarzhuk@gmail.com>
2024-12-17 12:47:45 +0100

    minus segfault in case no weapon is assigned to HumanB

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -8,7 +8,10 @@ HumanB::~HumanB()
 {
 }
 void HumanB::attack(){
-	std::cout << CYAN400 << this->_name << RESET << " attacks with their " << AMBER400 << this->_weapon->getType() << RESET <<std::endl;
+	if (!this->_weapon)
+		std::cout << CYAN400 << this->_name << RESET << AMBER400 << " has no weapon :(" << RESET <<std::endl;
+	else
+		std::cout << CYAN400 << this->_name << RESET << " attacks with their " << AMBER400 << this->_weapon->getType() << RESET <<std::endl;
 }
 void HumanB::setWeapon(Weapon &weapon){
 	this->_weapon = &weapon;
