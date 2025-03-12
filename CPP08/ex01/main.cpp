6bb9d696da5bbce9e78ab0ac00d84ca5b87fc82c
fraumarzhuk <fraumarzhuk@gmail.com>
2025-03-12 14:10:03 +0100

    ex02 has beatiful tests now

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -1,6 +1,6 @@
 #include "Span.hpp"
 #include <iostream>
-
+#include "colors.hpp"
 
 int main() {
 Span sp = Span(5);
@@ -12,13 +12,30 @@ sp.addNumber(11);
 std::cout << sp.shortestSpan() << std::endl;
 std::cout << sp.longestSpan() << std::endl;
 
-Span medium = Span(10000);
+Span medium = Span(100);
 medium.fillSpan(-520, 520);
 
 medium.printSpan();
+std::cout << BG_LIME300 PINK500 << "Normal Span: " << RESET << std::endl;
 std::cout << "shortest span: " << medium.shortestSpan() << std::endl;
 std::cout << "longest span: " << medium.longestSpan() << std::endl;
 
+Span empty = Span(0);
+try {
+	std::cout << BG_LIME300 PINK500 << "Empty Span: " << RESET << std::endl;
+	std::cout << "shortest span: " << empty.shortestSpan() << std::endl;
+	std::cout << "longest span: " << empty.longestSpan() << std::endl;
+} catch (std::exception &e) {
+	std::cerr << e.what() << std::endl;
+}
+Span empty2 = Span(2);
+try {
+	std::cout << BG_LIME300 PINK500 << "Span with 1 number: " << RESET << std::endl;
+	std::cout << "shortest span: " << empty2.shortestSpan() << std::endl;
+	std::cout << "longest span: " << empty2.longestSpan() << std::endl;
+} catch (std::exception &e) {
+	std::cerr << e.what() << std::endl;
+}
 return 0;
 
 }
