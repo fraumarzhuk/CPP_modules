617511ba6b96f01c428897a0f5749059d2d2afe1
Marianna Zhukova <mzhukova@c1r4s1.42wolfsburg.de>
2025-03-19 13:12:39 +0100

    start cpp09

---
+++ CPP09/ex00/main.cpp
@@ -0,0 +1,10 @@
+#include "BitcoinExchange.hpp"
+#include <vector>
+
+
+//TODO:
+//1. arguments check
+	//1.1 check name, format, file passed
+	//1.2 check that file can be open and not empty
+//2. contents of file check
+	//1. 
